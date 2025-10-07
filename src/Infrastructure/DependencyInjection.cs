74223f708c3e32f6a3f7e65bac93e0fd123b3581
Manuel <manuel.sharifi@gmail.com>
2025-10-07 17:13:14 +0200

    moved all Infrastructure Services into DependencyInjection, calling AddInfrastructure from Program.cs

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 using Infrastructure.Repositories;
+using Infrastructure.Repositories.Configurations;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure;
 
@@ -8,6 +10,13 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddSingleton<ITodoService, TodoService>();
+        services.AddOptions<SettingsOptions>()
+            .BindConfiguration(SettingsOptions.ConfigurationSectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddSingleton<IValidateOptions<SettingsOptions>, ValidateSettingsOptions>();
+        
         return services;
     }
 }
