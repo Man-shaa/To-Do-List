692045d7655fc8e1ee4555fe9250b697a5bc3d76
Manuel <manuel.sharifi@gmail.com>
2025-10-16 16:52:36 +0200

    Refactor snapshot tests to use NSubstitute instead of Moq for mocking ITodoService

--- tests/SnapshotTests/src/Application/Todos/Queries/GetTodo/GetTodoByIdHandlerSnapshotTests.cs
+++ tests/SnapshotTests/src/Application/Todos/Queries/GetTodo/GetTodoByIdHandlerSnapshotTests.cs
@@ -2,6 +2,7 @@ using Application.Todos.Queries.GetTodo;
 using Domain.Entities;
 using Infrastructure.Repositories;
 using Moq;
+using NSubstitute;
 
 namespace SnapshotTests.Application.Todos.Queries.GetTodo;
 
@@ -10,13 +11,17 @@ public class GetTodoByIdHandlerSnapshotTests
     [Fact]
     public async Task GetTodoByIdHandler_WithExistingTodo_ReturnsTodo()
     {
-        var todoServiceMock = new Mock<ITodoService>();
+        var todoServiceMock = Substitute.For<ITodoService>();
         var getTodoByIdQuery = new GetTodoByIdQuery(1);
-        var getTodoByIdHandler = new GetTodoByIdHandler(todoServiceMock.Object);
 
-        todoServiceMock.Setup(t => t.GetByIdAsync(1, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new Todo(1, "Snapshot Title 1", new Uri("https://localhost:7214/todos/1"), 1));
+        todoServiceMock.GetByIdAsync(1, Arg.Any<CancellationToken>())
+            .Returns(new Todo(
+                id: 1,
+                title: "Snapshot Title 1",
+                url: new Uri("https://localhost:7214/todos/1"),
+                order: 1));
 
+        var getTodoByIdHandler = new GetTodoByIdHandler(todoServiceMock);
         var sut = getTodoByIdHandler.Handle(getTodoByIdQuery, CancellationToken.None); 
 
         await Verify(sut);
@@ -25,12 +30,12 @@ public class GetTodoByIdHandlerSnapshotTests
     [Fact]
     public async Task GetTodoByIdHandler_TodoNotFound_ReturnsNull()
     {
-        var todoServiceMock = new Mock<ITodoService>();
+        var todoServiceMock = Substitute.For<ITodoService>();
         var getTodoByIdQuery = new GetTodoByIdQuery(1);
-        var getTodoByIdHandler = new GetTodoByIdHandler(todoServiceMock.Object);
+        var getTodoByIdHandler = new GetTodoByIdHandler(todoServiceMock);
 
-        todoServiceMock.Setup(t => t.GetByIdAsync(1, It.IsAny<CancellationToken>()))
-            .ReturnsAsync((Todo?)null);
+        todoServiceMock.GetByIdAsync(1, Arg.Any<CancellationToken>())
+            .Returns((Todo?)null);
 
         var sut = getTodoByIdHandler.Handle(getTodoByIdQuery, CancellationToken.None);
 
