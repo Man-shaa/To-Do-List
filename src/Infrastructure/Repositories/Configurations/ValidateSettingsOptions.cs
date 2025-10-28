af380500a1b9c0a6d692a65f565727506c318551
Manuel <manuel.sharifi@gmail.com>
2025-10-28 14:07:29 +0100

    tqt

--- src/Infrastructure/Repositories/Configurations/ValidateSettingsOptions.cs
+++ src/Infrastructure/Repositories/Configurations/ValidateSettingsOptions.cs
@@ -5,4 +5,8 @@ namespace Infrastructure.Repositories.Configurations;
 [OptionsValidator]
 public partial class ValidateSettingsOptions : IValidateOptions<SettingsOptions>
 {
+    public ValidateOptionsResult Validate(string? name, SettingsOptions options)
+    {
+        throw new NotImplementedException();
+    }
 }
