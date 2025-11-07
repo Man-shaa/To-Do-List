225f605906469dec0ff8c0b58cf91ffc5301c237
Manuel <manuel.sharifi@gmail.com>
2025-11-07 15:20:43 +0100

    Add integration tests for CreateTodo endpoint: Validation Error with Bad request returned when Title is empty

--- tests/Presentation.Tests/Endpoints/TodoEndpointsTests.cs
+++ tests/Presentation.Tests/Endpoints/TodoEndpointsTests.cs
@@ -7,12 +7,26 @@ namespace Presentation.Tests.Endpoints;
 [Collection("TodoTestingCollection")]
 public sealed class TodoEndpointsTests(TestingFixture fixture) : IClassFixture<TestingFixture>
 {
-    // Test for POST /todos
     [Fact]
     public async Task CreateTodo_ReturnsCreatedTodo()
     {
         var client = fixture.CreateHttpClient();
         var testTodo = new { Title = "Test Todo", Order = 1, Completed = false };
+
+        var sut = await client.PostAsJsonAsync("/todos", testTodo);
+
+        await Verify(new
+        {
+            StatusCode = sut.StatusCode,
+            Content = await sut.Content.ReadFromJsonAsync<Todo>()
+        });
+    }
+    
+    [Fact]
+    public async Task CreateTodo_ShouldReturnBadRequestWhenTitleIsEmpty()
+    {
+        var client = fixture.CreateHttpClient();
+        var testTodo = new { Title = "", Order = 1, Completed = false };
 
         var sut = await client.PostAsJsonAsync("/todos", testTodo);
 
