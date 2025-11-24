378cc5c444cd86d3168ef9822026be2ac77cce31
Manuel <manuel.sharifi@gmail.com>
2025-11-24 13:53:14 +0100

    WIP openTelemetry

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -2,15 +2,37 @@ using Application;
 using Infrastructure;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Presentation;
 using Presentation.Endpoints;
 using Presentation.ExceptionHandlers;
 
+const string serviceName = "Presentation";
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddPresentation()
     .AddInfrastructure()
     .AddApplication();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName))
+        .AddConsoleExporter();
+});
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter())
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter());
 
 var app = builder.Build();
 
