44b4f17f1e6901f56cab05ff40bd1341ae88b82e
Manuel <manuel.sharifi@gmail.com>
2025-12-02 11:40:28 +0100

    Add ApiRoutes class to define API route constants for versioning

---
+++ src/Presentation/Common/Constants/ApiRoutes.cs
@@ -0,0 +1,9 @@
+namespace Presentation.Common.Constants;
+
+public static class ApiRoutes
+{
+    public const string Root = "api/v{version:apiVersion}";
+    private const string RootWithoutVersioning = "api";
+    
+    
+}
